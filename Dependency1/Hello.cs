6a78b028236f19e558b8e266001d0f06aeb0a1bb
SpiderRock-Bill <50113699+SpiderRock-Bill@users.noreply.github.com>
2022-02-18 09:46:08 -0600

    dep 1

---
+++ Dependency1/Hello.cs
@@ -0,0 +1,13 @@
+﻿namespace Dependency1
+{
+    using MoreLinq;
+
+    public class Say
+    {
+        public static void Hello()
+        {
+            var version = new List<int>().Prepend(1).First();
+            System.Console.WriteLine($"  Dependency {version}");
+        }
+    }
+}
