d5a911e46d581732ebd3fb21491ea59af5ff0563
SpiderRock-Bill <50113699+SpiderRock-Bill@users.noreply.github.com>
2022-02-18 09:45:18 -0600

    dependencies

---
+++ Dependency2/Hello.cs
@@ -0,0 +1,15 @@
+﻿namespace Dependency2
+{
+    using MoreLinq;
+
+    public class Say
+    {
+        public static void Hello()
+        {
+            Dependency1.Say.Hello();
+
+            var version = new List<int>().Prepend(2).First();
+            System.Console.WriteLine($"  Dependency {version}");
+        }
+    }
+}
